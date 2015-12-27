9488eb2cb33fbcd538e18587bff244a8a5929d2a
moliva <oliva.miguelh@gmail.com>
2015-12-27 18:42:36 -0300

    Adding basic db interface

---
+++ src/clj/songbook/db.clj
@@ -0,0 +1,11 @@
+(ns songbook.db
+  (:require [somnium.congomongo :as mongo]))
+
+(def conn
+  (mongo/make-connection "songbook"
+                         :host "127.0.0.1"
+                         :port 27017))
+
+(defn get-user [username]
+  (mongo/with-mongo conn
+    (mongo/fetch-one :users :where {:username username})))
